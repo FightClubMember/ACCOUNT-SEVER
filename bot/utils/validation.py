ffec11233b5746e729bb5118cc8e66deaf8e919c
Himanshu Kumar <himanshu20051124@gmail.com>
2026-07-15 14:47:49 +0530

    Support email plus subaddressing in email validation

--- bot/utils/validation.py
+++ bot/utils/validation.py
@@ -5,8 +5,8 @@ EMAIL_REGEX = re.compile(
     r"^(iiacc)?|[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
 )
 
-# A clean standard regex check
-STANDARD_EMAIL_REGEX = re.compile(r"^[\w\.-]+@[\w\.-]+\.\w+$")
+# A clean standard regex check supporting plus addressing
+STANDARD_EMAIL_REGEX = re.compile(r"^[\w\.\+-]+@[\w\.-]+\.\w+$")
 
 def validate_email(email: str) -> bool:
     """Returns True if the email is in a valid format, False otherwise."""
